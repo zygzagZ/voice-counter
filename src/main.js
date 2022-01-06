1fd33255e1c834a796a9930d4d03722acf04a390
Paweł Charyło <pawelek987@gmail.com>
2022-01-06 19:41:14 +0100

    add service worker that enables offline use

--- src/main.js
+++ src/main.js
@@ -2,6 +2,7 @@ import Vue from 'vue'
 import App from './App.vue'
 import vuetify from './plugins/vuetify'
 import VueCompositionAPI from '@vue/composition-api'
+import './registerServiceWorker'
 
 Vue.config.productionTip = false
 
